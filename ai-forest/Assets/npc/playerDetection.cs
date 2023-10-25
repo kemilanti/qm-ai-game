120a47cdc4b6d5ab049c138f189162d3c8bb7713
WMWMW1 <91670603+kemilanti@users.noreply.github.com>
2023-10-25 16:29:16 +0100

    .

---
+++ ai-forest/Assets/npc/playerDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDetection : MonoBehaviour
+{
+    bool playerdetection = false;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerdetection && Input.GetKeyDown(KeyCode.F))
+        {
+            print("阿巴阿巴");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "PlayerArmature")
+
+        playerdetection = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        playerdetection = false;            
+    }
+}
