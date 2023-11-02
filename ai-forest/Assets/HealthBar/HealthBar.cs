f09e0140425c0c7e1c5fe6fd04e3a14f30a2d3a4
WeitaoDeng <ec21834@qmul.ac.uk>
2023-11-02 21:37:23 +0000

    health bar fix
    1

--- ai-forest/Assets/HealthBar/HealthBar.cs
+++ ai-forest/Assets/HealthBar/HealthBar.cs
@@ -29,19 +29,19 @@ public class HealthBar : MonoBehaviour
         // BarFiller();
     }
 
-<<<<<<< Updated upstream
-    // private void BarFiller()
-    // {
-    //     // 使用 Mathf.Lerp 平滑地更新血条值
-    //     Bar.fillAmount = Mathf.Lerp(Bar.fillAmount, Health / maxHealth, lerpSpeed * Time.deltaTime);
-    // }
-=======
-    private void BarFiller()
-    {
-        // Smoothly update blood bar values using Mathf.Lerp
-        Bar.fillAmount = Mathf.Lerp(Bar.fillAmount, Health / maxHealth, lerpSpeed * Time.deltaTime);
-    }
->>>>>>> Stashed changes
+// <<<<<<< Updated upstream
+//     // private void BarFiller()
+//     // {
+//     //     // 使用 Mathf.Lerp 平滑地更新血条值
+//     //     Bar.fillAmount = Mathf.Lerp(Bar.fillAmount, Health / maxHealth, lerpSpeed * Time.deltaTime);
+//     // }
+// =======
+//     private void BarFiller()
+//     {
+//         // Smoothly update blood bar values using Mathf.Lerp
+//         Bar.fillAmount = Mathf.Lerp(Bar.fillAmount, Health / maxHealth, lerpSpeed * Time.deltaTime);
+//     }
+// >>>>>>> Stashed changes
 
     public void AddHealth()
     {
